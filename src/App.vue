78bef7704f70b82452da15591ca2bfaa2945e931
unknown <judejulius12@gmail.com>
2022-04-08 11:07:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <label for="number">Input Number: </label>
+    <input type="number" v-model="limit" />
+    <Numbers/>
+  </div>
+</template>
+
+<script>
+import Numbers from '@/components/Numbers';
+
+export default {
+  name: 'App',
+  components: {Numbers},
+  data()
+  {
+    return {
+      limit: 100
+    }
+  }
+}
+</script>
+
+<style scoped>
+input{
+  height: 20px;
+  width: 210px;
+  font-size: 1rem;
+}
+label{
+  font-family: sans-serif;
+}
+</style>
---
+++ src/components/Numbers.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="number" :id="'number'+number" v-for="number in n()" :key="number" @mouseover="hov(number)" @mouseout="reset">
+      {{number}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data()
+  {
+    return {
+      limit: this.$parent.limit,
+      numbers: []
+    }
+  },
+  watch: {
+    ['$parent.limit'](newLimit)
+    {
+      this.limit = newLimit;
+    }
+  },
+  methods: {
+    //Naming conventions need to be better
+    n()
+    {
+      let numbers = [];
+      for(var i = 0; i < this.limit; i++)
+      {
+        // numbers = [...numbers, i];
+        numbers.push(i)
+      }
+      return numbers.sort(() => Math.random() - 0.5);
+    },
+    //Naming conventions need to be better
+    hov(number)
+    {
+      //DOM we dont use in vue
+      const nums = document.querySelectorAll('.number');
+    //maybe use a v model
+      for(let i = 0; i < nums.length; i++)
+      {
+        const num = nums[i].textContent.trim();
+        if(number % num === 0)
+        {
+          nums[i].classList.add('active')
+          console.log('divisor', num)
+        }
+      }
+    },
+    reset()
+    {
+      const nums = document.querySelectorAll('.number');
+      nums.forEach(num => num.classList.remove('active'))
+    }
+  }
+}
+</script>
+
+<style scoped>
+	.number {
+		display: inline-block;
+		padding: 5px;
+		background-color: lightgrey;
+		margin: 5px;
+	}
+
+	.active {
+		background-color: red;
+	}
+  div{
+    font-size: 2rem;
+    font-family: sans-serif;
+  }
+  input{
+    height: 30px;
+  }
+
+</style>
